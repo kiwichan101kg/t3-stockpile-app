b8744993fa5967beb3d8444941426c9a6ec307b6
kjnhk1001 <kjn.hk10001@gmail.com>
2024-09-19 22:53:16 +0900

    fix：レイアウト崩れ修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { api, HydrateClient } from "@/trpc/server";
 import { getServerAuthSession } from "@/server/auth";
 import { getOgps } from "@/lib/getOgp";
 import { Logo } from "@/components/Logo";
+import { Footer } from "@/components/Footer";
 
 type Article = {
   id: string;
@@ -66,32 +67,7 @@ export default async function Home() {
         <UrlBox />
         <ArticleList articleList={enrichedArticleList} />
       </main>
-      <footer className="border-t border-gray-200 bg-white py-4">
-        <div className="container mx-auto flex flex-col items-center justify-between md:flex-row">
-          {/* コピーライト部分 */}
-          <p className="text-sm text-gray-600">
-            © {new Date().getFullYear()} Tech📚Stock. All rights reserved.
-          </p>
-
-          {/* リンク部分 */}
-          <div className="mt-4 flex space-x-6 md:mt-0">
-            <Link
-              href={"/info/terms"}
-              target="_blank"
-              className="text-sm text-gray-600 transition hover:text-gray-900"
-            >
-              利用規約
-            </Link>
-            <Link
-              href={"/info/privacy-policy"}
-              target="_blank"
-              className="text-sm text-gray-600 transition hover:text-gray-900"
-            >
-              プライバシーポリシー
-            </Link>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </HydrateClient>
   );
 }
