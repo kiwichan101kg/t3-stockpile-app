3ec2f3c31002488b08e0a0f80b9bbec4786863d2
kjnhk1001 <kjn.hk10001@gmail.com>
2024-09-23 22:08:01 +0900

    OGP追加

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,8 +4,9 @@ import React from "react";
 export const Logo = () => {
   return (
     <Link href="/" className="cursor-pointer">
-      <h1 className="text-2xl font-bold text-gray-800 sm:text-3xl">
-        Tech📚Stock
+      <h1 className="font-serif text-2xl font-extrabold text-gray-800 sm:text-3xl">
+        <span className="text-gray-900">Tech📚</span>
+        <span className="text-blue-500">Stock</span>
       </h1>
     </Link>
   );
